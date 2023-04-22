76495ee3f47a8a9bca6234171e033811ea9f72f3
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-04-22 08:07:05 +0100

    Chess layer & Printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,18 @@
 package boardgame;
 
 public class Piece {
+	protected Position position;
+	private Board board;
+	
+	//constructor without 'position' because the first position of a piece is 'null'
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
 
+	protected Position getPosition() {
+		return position;
+	}
+	
+	
 }
